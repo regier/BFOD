32926c9bd8aacb8b607248da4acd78852d5bf9b4
Regier Kunkel <regier.kunkel@gmail.com>
2020-05-12 16:14:53 +0100

    Version bump. Adding export to variables. Make script create folders.

--- Building_FlightGear_OnDebian.sh
+++ Building_FlightGear_OnDebian.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
+
 # Welcome to Building FlightGear OnDebian
 # This is the following version of this script.
-bfod_ver="0.0.1.20200512.1450"
+bfod_ver="0.0.1.20200512.1610"
 
 # This script's intention is to download and build
 # dependencies required to build FlightGear and
@@ -15,30 +17,33 @@ bfod_ver="0.0.1.20200512.1450"
 # simply edit the variables bellow.
 
 
-#!/bin/bash
 
 # A folder called Games will be created in the users home if it doesn't exist
 # and FlightGear will be installed there.
-fg_install_dir="$HOME/Games/FlightGear"
+export fg_install_dir="$HOME/Games/FlightGear"
 
 # This is the location for source codes and temp files.
-fg_temp_files="/dev/shm/FlightGear"
+export fg_temp_files="/dev/shm/FlightGear"
 
 # List of addresses and versions for FlightGear, in the
 # following order. PLib, OpenSceneGraph, SimGear.
 
 # PLib
-plib_svn="svn://svn.code.sf.net/p/plib/code/trunk"
+export plib_svn="svn://svn.code.sf.net/p/plib/code/trunk"
 
 # OpenSceneGraph
-osg_git="https://github.com/openscenegraph/OpenSceneGraph"
-osg_version="OpenSceneGraph-3.6"
+export osg_git="https://github.com/openscenegraph/OpenSceneGraph"
+export osg_version="OpenSceneGraph-3.6"
 
 # SimGear
-opensim_git="git://git.code.sf.net/p/flightgear/simgear"
+export opensim_git="git://git.code.sf.net/p/flightgear/simgear"
 
 # FlightGear
-flghtgear_git="git://git.code.sf.net/p/flightgear/flightgear"
+export flghtgear_git="git://git.code.sf.net/p/flightgear/flightgear"
 
 # FlightGear Data
-flghtgear_git="git://git.code.sf.net/p/flightgear/fgdata"
+export flghtgear_git="git://git.code.sf.net/p/flightgear/fgdata"
+
+# Creating temp folder and instalation folder as define on the first variables
+# of this file.
+mkdir -p $fg_install_dir $fg_temp_files
