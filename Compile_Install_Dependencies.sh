ab12892276877ea9b7d5f0191dfdb4f420767211
Regier Kunkel <regier.kunkel@gmail.com>
2020-05-12 16:34:27 +0100

    Version bump. Added Compile_Install_Dependencies.sh to compile everything.

---
+++ Compile_Install_Dependencies.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Welcome to Building FlightGear OnDebian
+# This is the following version of this script.
+bfod_ver="0.0.1.20200512.1640"
+
+# This script's intention is to download and build
+# dependencies required to build FlightGear and
+# FlightGear itself.
+
+# The script follows the KISS principle, each file does one thing only.
+
+# This file Compiles everything.
