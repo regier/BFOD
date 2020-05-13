4fc7a81a95cf7374c644ccc6e2a86bfa945dbd7a
Regier Kunkel <regier.kunkel@gmail.com>
2020-05-13 01:39:05 +0100

    Added script that will clear compiled code.

---
+++ Clear_Temp.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Welcome to Building FlightGear OnDebian
+# This is the following version of this script.
+bfod_ver="0.0.1.20200513.1110"
+
+# This script's intention is to download and build
+# dependencies required to build FlightGear and
+# FlightGear itself.
+
+# The script follows the KISS principle, each file does one thing only.
+
+# This file Deletes all built code aka make clean.
+
